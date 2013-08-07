2056f41ce6e16ae2d438a2dd6c8dd53cb9e9c1f5
Damian Mooyman <damian.mooyman@gmail.com>
2013-08-08 11:16:35 +1200

    Added recaptcha to comments form

--- mysite/_config.php
+++ mysite/_config.php
@@ -20,5 +20,11 @@ if (!Director::isDev()) {
     SS_Log::add_writer(new SS_LogEmailWriter('damian.mooyman@gmail.com'), SS_Log::ERR);
 }
 
+// Setup recaptcha from _ss_environment
+if(defined('SS_RECAPTCHA_PUBLIC') && defined('SS_RECAPTCHA_PRIVATE')) {
+	RecaptchaField::$public_api_key = SS_RECAPTCHA_PUBLIC;
+	RecaptchaField::$private_api_key = SS_RECAPTCHA_PRIVATE;
+	SpamProtectorManager::set_spam_protector('RecaptchaProtector');
+}
 
 Commenting::set_config_value('SiteTree', 'use_gravatar', true);
